de7762401a418a8c4b073bfbdab9906f47e305a0
bfinnema <bfinnema@cisco.com>
2020-12-04 09:01:14 +0100

    Added python code to the register-license service to use the license server API to get tokens.

--- lic-server-token.py
+++ lic-server-token.py
@@ -1,45 +1,124 @@
 import requests
 import urllib3
 import json
+import sys
 
 urllib3.disable_warnings()
-url = "https://10.101.1.107:8443/oauth/token"
+
+client_id = "CMWk9L0mJ6Ynn8SwEnjd32-f_IqlNXWczjV6nC74ve7tFR--1c0LW8qp18N5_y1G"
+client_secret = "7vtLXBnNzDXJ7wu5j9YsmV0cb24YEBKoZFvVihrbbt7yt677fAfZuwsUrKOi8Lvv"
+host = "10.101.1.107"
+port = "8443"
+smartAccountName = "InternalTestDemoAccount9.cisco.com"
+virtualAccountName = "Default"
+accessCodeOk = True
+
+print("********** GET AN ACCESS_TOKEN *********")
+url = f'https://{host}:{port}/oauth/token'
+print("url: {}".format(url))
 
 payload = {
-    "client_id": "efcce99b38b5ea24afff161a3940231e9fa8f8eaffe1e911edec82b0989a1517",
-    "client_secret": "dd15ea6a89f7032e3bf117f4e48fb3a73d2234f57151be3100a036659e153aa7",
+    "client_id": client_id,
+    "client_secret": client_secret,
     "grant_type": "client_credentials"
 }
-
 headers = {
   'Content-Type': 'application/json'
 }
 
-response = requests.request("POST", url, headers=headers, data = json.dumps(payload), verify = False)
-jsonResponse = response.json()
-print("Entire JSON response")
-print(jsonResponse)
-access_token = jsonResponse["access_token"]
-print("access_token: ", access_token)
+try:
+    response = requests.request("POST", url, headers=headers, data = json.dumps(payload), verify = False)
+    status_code = response.status_code
+    print(f'status_code: {status_code}')
+    if (status_code == 200):
+        jsonResponse = response.json()
+        print("Entire JSON response")
+        print(jsonResponse)
+        access_token = jsonResponse["access_token"]
+        print("access_token: ", access_token)
+    else:
+        print(f'Something went wrong: {status_code}')
+        accessCodeOk = False
+except requests.exceptions.HTTPError as err:
+    print ("Error in connection --> "+str(err))
+    accessCodeOk = False
+    sys.exit()
+
+print("*******************")
+print("********** GET ALL VALID EXISTING TOKENS *********")
 print("*******************")
 
-url = "https://10.101.1.107:8443/api/v1/accounts/CISCO%20Danish%20LAB/virtual-accounts/Default/tokens"
+if (accessCodeOk):
+    # url = "https://10.101.1.107:8443/api/v1/accounts/CISCO%20Danish%20LAB/virtual-accounts/Default/tokens"
+    url = f'https://{host}:{port}/api/v1/accounts/{smartAccountName}/virtual-accounts/{virtualAccountName}/tokens'
+    print(f"url: {url}")
 
-payload = {
-    "expiresAfterDays": 1,
-    "description": "Test VA Creation",
-    "exportControlled": "Allowed"
-}
+    payload = {}
+    headers = {
+    'Authorization': 'Bearer '+access_token
+    }
 
-headers = {
-  'Content-Type': 'application/json',
-  'Authorization': 'Bearer '+access_token
-}
+    try:
+        response = requests.request("GET", url, headers=headers, data = payload, verify = False)
+        status_code = response.status_code
+        print(f'status_code: {status_code}')
+        if (status_code == 200):
+            jsonResponse = response.json()
+            print("Entire JSON response")
+            print(jsonResponse)
+            print("Length of Token List: ", len(jsonResponse["tokens"]))
+            numTokens = len(jsonResponse["tokens"])
+            if numTokens>0:
+                register_token = jsonResponse["tokens"][0]["token"]
+                print("First token: ", register_token)
+                regTokenOK = True
+            else:
+                regTokenOK = False
+        else:
+            print(f'Something went wrong: {status_code}')
+            regTokenOK = False
+    except requests.exceptions.HTTPError as err:
+        print ("Error in connection --> "+str(err))
+        regTokenOK = False
+        sys.exit()
 
-response = requests.request("POST", url, headers=headers, data = json.dumps(payload), verify = False)
-jsonResponse = response.json()
-print("Entire JSON response")
-print(jsonResponse)
-token = jsonResponse["tokenInfo"]["token"]
-print("Token: ", token)
 print("*******************")
+print("********** GENERATE A NEW TOKEN *********")
+print("*******************")
+
+if (regTokenOK == False):
+    # url = "https://10.101.1.107:8443/api/v1/accounts/CISCO%20Danish%20LAB/virtual-accounts/Default/tokens"
+    url = f'https://{host}:{port}/api/v1/accounts/{smartAccountName}/virtual-accounts/{virtualAccountName}/tokens'
+    print("url: " ,url)
+
+    payload = {
+        "expiresAfterDays": 1,
+        "description": "Test VA Creation",
+        "exportControlled": "Allowed"
+    }
+
+    headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer '+access_token
+    }
+
+    try:
+        response = requests.request("POST", url, headers=headers, data = json.dumps(payload), verify = False)
+        status_code = response.status_code
+        print(f'status_code: {status_code}')
+        if (status_code == 200):
+            jsonResponse = response.json()
+            print("Entire JSON response")
+            print(jsonResponse)
+            register_token = jsonResponse["tokenInfo"]["token"]
+            print("Token: ", register_token)
+            print("*******************")
+        else:
+            print(f'Something went wrong: {status_code}')
+            accessCodeOk = False
+    except requests.exceptions.HTTPError as err:
+        print ("Error in connection --> "+str(err))
+        regTokenOK = False
+        sys.exit()
+    finally:
+        if response : response.close()
--- register-license/python/register_license/main.py
+++ register-license/python/register_license/main.py
@@ -1,7 +1,10 @@
 # -*- mode: python; python-indent: 4 -*-
 import ncs
 from ncs.application import Service
-
+import requests
+import urllib3
+import json
+import sys
 
 # ------------------------
 # SERVICE CALLBACK EXAMPLE
@@ -17,7 +20,116 @@ class ServiceCallbacks(Service):
         device_group = service.device_group
         self.log.info('*** Register_license, device-group: ', device_group)
         idtoken = root.devreglic.inventory.device_groups.device_group[device_group].idtoken
-        self.log.info('*** Register_license, idtoken: ', idtoken)
+        self.log.info('*** Register_license, idtoken from Inventory: ', idtoken)
+
+
+        urllib3.disable_warnings()
+
+        client_id = "CMWk9L0mJ6Ynn8SwEnjd32-f_IqlNXWczjV6nC74ve7tFR--1c0LW8qp18N5_y1G"
+        client_secret = "7vtLXBnNzDXJ7wu5j9YsmV0cb24YEBKoZFvVihrbbt7yt677fAfZuwsUrKOi8Lvv"
+        host = "10.101.1.107"
+        port = "8443"
+        smartAccountName = "InternalTestDemoAccount9.cisco.com"
+        virtualAccountName = "Default"
+        accessCodeOk = True
+
+        # url = f'https://{host}:{port}/oauth/token'
+        url = "https://" + host + ":" + port + "/oauth/token"
+        self.log.info("access_token url: ", url)
+
+        payload = {
+            "client_id": client_id,
+            "client_secret": client_secret,
+            "grant_type": "client_credentials"
+        }
+        headers = {
+        'Content-Type': 'application/json'
+        }
+
+        try:
+            response = requests.request("POST", url, headers=headers, data = json.dumps(payload), verify = False)
+            status_code = response.status_code
+            self.log.info("status_code: ", status_code)
+            if (status_code == 200):
+                jsonResponse = response.json()
+                access_token = jsonResponse["access_token"]
+                self.log.info("access_token: ", access_token)
+            else:
+                self.log.info('Something went wrong: ', status_code)
+                accessCodeOk = False
+        except requests.exceptions.HTTPError as err:
+            self.log.info("Error in connection --> "+str(err))
+            accessCodeOk = False
+
+        if (accessCodeOk):
+            # url = f'https://{host}:{port}/api/v1/accounts/{smartAccountName}/virtual-accounts/{virtualAccountName}/tokens'
+            url = "https://" + host + ":" + port + "/api/v1/accounts/" + smartAccountName + "/virtual-accounts/" + virtualAccountName + "/tokens"
+            self.log.info("token url: ", url)
+
+            payload = {}
+            headers = {
+            'Authorization': 'Bearer '+access_token
+            }
+
+            try:
+                response = requests.request("GET", url, headers=headers, data = payload, verify = False)
+                status_code = response.status_code
+                self.log.info("status_code: ", status_code)
+                if (status_code == 200):
+                    jsonResponse = response.json()
+                    self.log.info("Length of Token List: ", len(jsonResponse["tokens"]))
+                    numTokens = len(jsonResponse["tokens"])
+                    if numTokens>0:
+                        register_token = jsonResponse["tokens"][0]["token"]
+                        self.log.info("First token: ", register_token)
+                        regTokenOK = True
+                    else:
+                        regTokenOK = False
+                else:
+                    self.log.info('Something went wrong: ', status_code)
+                    regTokenOK = False
+            except requests.exceptions.HTTPError as err:
+                self.log.info("Error in connection --> "+str(err))
+                regTokenOK = False
+
+        if (regTokenOK == False):
+            # url = f'https://{host}:{port}/api/v1/accounts/{smartAccountName}/virtual-accounts/{virtualAccountName}/tokens'
+            url = "https://" + host + ":" + port + "/api/v1/accounts/" + smartAccountName + "/virtual-accounts/" + virtualAccountName + "/tokens"
+            self.log.info("generate token url: ", url)
+
+            payload = {
+                "expiresAfterDays": 1,
+                "description": "Test VA Creation",
+                "exportControlled": "Allowed"
+            }
+
+            headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer '+access_token
+            }
+
+            try:
+                response = requests.request("POST", url, headers=headers, data = json.dumps(payload), verify = False)
+                status_code = response.status_code
+                self.log.info("status_code: ", status_code)
+                if (status_code == 200):
+                    jsonResponse = response.json()
+                    register_token = jsonResponse["tokenInfo"]["token"]
+                    self.log.info("Token: ", register_token)
+                    idtoken = register_token
+                else:
+                    self.log.info('Something went wrong: ', status_code)
+            except requests.exceptions.HTTPError as err:
+                self.log.info("Error in connection --> "+str(err))
+                regTokenOK = False
+                sys.exit()
+            finally:
+                if response : response.close()
+        else:
+            idtoken = register_token
+
+
+        self.log.info('*** Register_license, idtoken from License Server: ', idtoken)
 
         with ncs.maapi.single_read_trans('admin', 'python') as t: 
             root = ncs.maagic.get_root(t) 
